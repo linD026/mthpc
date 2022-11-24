5cb4a1abbf2a66ffec085249de08596114488bf0
Chih-En Lin <shiyn.lin@gmail.com>
2022-11-25 04:03:01 +0800

    debug: Introduce debug - printdg and dump_stack

--- include/mthpc/debug.h
+++ include/mthpc/debug.h
@@ -1,25 +1,45 @@
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
+#ifndef __MTHPC_DEBUG_H__
+#define __MTHPC_DEBUG_H__
 
 #include <stdio.h>
 
-#define debug_err_stream stderr
+#include <util.h>
 
-#define BUG_ON(cond, fmt, ...)                                         \
-    do {                                                               \
-        if ((cond)) {                                                  \
-            fprintf(debug_err_stream, "%s:%d:%s: " fmt "\n", __FILE__, \
-                    __LINE__, __func__, ##__VA_ARGS__);                \
-            exit(EXIT_FAILURE);                                        \
-        }                                                              \
+#define mthpc_debug_stream stderr
+
+#define mtphc_printdg(fmt, ...)                                                \
+    do {                                                                       \
+        fprintf(mthpc_debug_stream, "%s:%d:%s: " fmt "\n", __FILE__, __LINE__, \
+                __func__, ##__VA_ARGS__);                                      \
     } while (0)
 
-#define WARN_ON(cond, fmt, ...)                                        \
-    do {                                                               \
-        if ((cond)) {                                                  \
-            fprintf(debug_err_stream, "%s:%d:%s: " fmt "\n", __FILE__, \
-                    __LINE__, __func__, ##__VA_ARGS__);                \
-        }                                                              \
+#define MTHPC_BUG_ON(cond, fmt, ...)                            \
+    do {                                                        \
+        if ((cond)) {                                           \
+            mthpc_printdg(fmt, __VA_ARGS__) exit(EXIT_FAILURE); \
+            exit(EXIT_FAILURE);                                 \
+        }                                                       \
     } while (0)
 
-#endif /* __DEBUG_H__ */
+#define MTHPC_WARN_ON(cond, fmt, ...)       \
+    do {                                    \
+        if ((cond))                         \
+            mthpc_printdg(fmt, __VA_ARGS__) \
+    } while (0)
+
+mthpc_always_inline void mthpc_dump_stack(void)
+{
+    void **stack_info;
+    int nr = 0;
+    void *buf[32];
+
+    nr = backtrace(buf, STACK_BUF_SIZE);
+    stack_info = backtrace_symbols(buf, nr);
+
+    mthpc_printdg("========== dump stack start ==========\n");
+    for (int i = 0; i < nr; i++)
+        mthpc_printdg("    %s\n", stack_info[i])
+            mthpc_printdg("========== dump stack  end  ==========\n");
+}
+
+#endif /* __MTHPC_DEBUG_H__*/
