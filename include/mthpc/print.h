0a73f67b1c6323c7d8b336f746e6a4717d3c9df4
Chih-En Lin <shiyn.lin@gmail.com>
2023-03-06 08:19:17 +0800

    print: Fix the warning of unmatch type of tv_{usec, sec}

--- include/mthpc/print.h
+++ include/mthpc/print.h
@@ -22,23 +22,24 @@ static inline struct timeval ___mthpc_get_us(void)
         fprintf(mthpc_debug_stream, fmt, ##__VA_ARGS__); \
     } while (0)
 
-#define mthpc_pr_info(fmt, ...)                                               \
-    do {                                                                      \
-        struct timeval __pr_tv = ___mthpc_get_us();                           \
-        mthpc_print("\e[32m[%5lu.%06lu]\e[0m %s:%d:%s: " fmt, __pr_tv.tv_sec, \
-                    __pr_tv.tv_usec, __FILE__, __LINE__, __func__,            \
-                    ##__VA_ARGS__);                                           \
+#define mthpc_pr_info(fmt, ...)                                         \
+    do {                                                                \
+        struct timeval __pr_tv = ___mthpc_get_us();                     \
+        mthpc_print("\e[32m[%5lu.%06lu]\e[0m %s:%d:%s: " fmt,           \
+                    (unsigned long)__pr_tv.tv_sec,                      \
+                    (unsigned long)__pr_tv.tv_usec, __FILE__, __LINE__, \
+                    __func__, ##__VA_ARGS__);                           \
     } while (0)
 
-#define mthpc_pr_err(fmt, ...)                                               \
-    do {                                                                     \
-        struct timeval __pr_tv = ___mthpc_get_us();                          \
-        fprintf(mthpc_err_stream,                                            \
-                "\e[32m[%5lu.%06lu] [TASK %lx]\e[0m %s:%d:%s: "              \
-                "\e[31m" fmt "\e[0m",                                        \
-                __pr_tv.tv_sec, __pr_tv.tv_usec,                             \
-                (unsigned long)pthread_self(), __FILE__, __LINE__, __func__, \
-                ##__VA_ARGS__);                                              \
+#define mthpc_pr_err(fmt, ...)                                                 \
+    do {                                                                       \
+        struct timeval __pr_tv = ___mthpc_get_us();                            \
+        fprintf(mthpc_err_stream,                                              \
+                "\e[32m[%5lu.%06lu] [TASK %lx]\e[0m %s:%d:%s: "                \
+                "\e[31m" fmt "\e[0m",                                          \
+                (unsigned long)__pr_tv.tv_sec, (unsigned long)__pr_tv.tv_usec, \
+                (unsigned long)pthread_self(), __FILE__, __LINE__, __func__,   \
+                ##__VA_ARGS__);                                                \
     } while (0)
 
 #endif /* __MTHPC_PRINT_H__ */
